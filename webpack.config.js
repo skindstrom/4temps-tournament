ce1028ae387a7276bdecc1fd9fa8c7a0dca27159
Simon Kindström <simon.kindstrom@gmail.com>
2017-12-10 17:15:38 +0100

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+/*
+Used to remove warning:
+WARNING in ./node_modules/express/lib/view.js
+81:13-25 Critical dependency: the request of a dependency is an expression
+*/
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    entry: './server/index.js',
+    target: 'node',
+    externals: [nodeExternals()],
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'lib')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+};
