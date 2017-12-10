80b2bba41656b576f359ce490c704cce6bf32610
Simon Kindström <simon.kindstrom@gmail.com>
2017-12-10 16:54:53 +0100

    Added flow

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
+// @flow
+
 import express from 'express';
+import type { $Request, $Response } from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req: $Request, res: $Response) => res.send('Hello world!'));
 app.listen(3000, () => console.log('Started express server'));
