a47b1f609ada220b14fedf731f24bfac53f5b3ce
Fredrik Rahn <lillrahn@live.se>
2017-12-14 16:55:09 +0100

    Fixed merge conflicts

--- src/app/components/App/index.js
+++ src/app/components/App/index.js
@@ -1,4 +1,3 @@
-<<<<<<< 6d35ee7693ac5ede6e92bba571935962e3fd2b26
 // @flow
 import React from 'react';
 import { Header } from 'semantic-ui-react';
@@ -17,25 +16,4 @@ const App = () => {
   );
 }
 
-=======
-// @flow
-import React from 'react';
-import { Header } from 'semantic-ui-react';
-import { Switch, Route } from 'react-router-dom';
-
-import Router from '../Router';
-import NavigationBar from '../NavigationBar';
-
-
-const App = () => {
-    return (
-        <div>
-            <Header as='h1'> 4 Temps tournament website </Header>
-            <NavigationBar />
-            <Router />
-        </div>
-    );
-}
-
->>>>>>> Moved components to router
 export default App;
