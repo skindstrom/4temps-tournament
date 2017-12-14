d8fa55c669b3046d75e065779cf8f93dfd15b3f2
Fredrik Rahn <lillrahn@live.se>
2017-12-14 15:05:59 +0100

    Moved components to router

--- src/app/components/App/index.js
+++ src/app/components/App/index.js
@@ -1,33 +1,17 @@
 // @flow
 import React from 'react';
 import { Header } from 'semantic-ui-react';
-import { Switch, Route } from 'react-router-dom';
 
+import Router from '../Router';
 import NavigationBar from '../NavigationBar';
-import CreateTournament from '../CreateTournament';
-import SignUp from '../SignUp';
 
-const Home = () => 'Home!';
 
 const App = () => {
   return (
     <div>
       <Header as='h1'> 4 Temps tournament website </Header>
       <NavigationBar />
-      <Switch>
-        <Route
-          /* match both '/' and '/home' */
-          path='/(|home)'
-          /* '/' will match everything, make sure to be exact */
-          exact
-          component={Home}
-        />
-        <Route
-          path='/create-tournament'
-          render={() => <CreateTournament user={null} />}
-        />
-        <Route path='/signup' component={SignUp} />
-      </Switch>
+      <Router />
     </div>
   );
 }
