20a2a95b2d3322119eaa0261b0ab32cf7ec4b140
Fredrik Rahn <lillrahn@live.se>
2017-12-14 15:05:35 +0100

    Added router

---
+++ src/app/components/Router/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// Components
+import CreateTournament from '../CreateTournament';
+import SignUp from '../SignUp';
+import Home from '../Home';
+
+
+const Router = () => {
+    return(
+        <router>
+            <Switch>
+                <Route path='/(|home)' exact component={Home} />
+                <Route path='/create-tournament' render={(props) => <CreateTournament user={null} />} />
+                <Route path='/signup' component={SignUp} />
+            </Switch>
+        </router>
+    )
+}
+
+export default Router;
