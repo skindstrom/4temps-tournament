97a54bd55b943f0acdb9a335dcced7a904d8df9c
Simon Kindström <simon.kindstrom@gmail.com>
2017-12-19 15:02:15 +0100

    Only navigate from navbar if not current path
    To not fill the history with the same path.
    Also added sign in and login button

--- src/app/components/NavigationBar/index.js
+++ src/app/components/NavigationBar/index.js
@@ -1,41 +1,86 @@
 // @flow
-import React from 'react';
-import { Menu, MenuItem } from 'semantic-ui-react';
-import { withRouter, Link } from 'react-router-dom';
-import type { Location } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Button, Menu, MenuItem, MenuMenu } from 'semantic-ui-react';
+import { withRouter } from 'react-router-dom';
+import type { Location, RouterHistory } from 'react-router-dom';
 
 type Props = {
-  location: Location
+  location: Location,
+  history: RouterHistory
 }
 
-const NavigationBar = ({ location }: Props) => {
-  const matches = location.pathname.match(/\/(.+)\/?\??.*/);
-  let activeName = 'home';
-  if (matches) {
-    activeName = matches[1];
+type State = {
+  activeName: string
+}
+
+class NavigationBar extends Component<Props, State> {
+  static _getActiveName = (location: string): string => {
+    const matches = location.match(/\/(.+)\/?\??.*/);
+    let activeName = 'home';
+    if (matches) {
+      activeName = matches[1];
+    }
+    return activeName;
   }
-  
-  return (
-    <Menu>
-      <Link to='/'>
+
+  state = {
+    activeName: NavigationBar._getActiveName(this.props.location.pathname)
+  };
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      activeName: NavigationBar._getActiveName(nextProps.location.pathname)
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.activeName != this.state.activeName;
+  }
+
+  _navigate = (to: string) => {
+    if (to != this.props.location.pathname) {
+      this.props.history.push(to);
+    }
+  }
+
+  render() {
+    return (
+      <Menu>
         <MenuItem
           name='home'
-          active={activeName==='home'}
+          active={this.state.activeName === 'home'}
+          onClick={() => this._navigate('/')}
         >
           Home
         </MenuItem>
-      </Link>
-      <Link to='/create-tournament'>
         <MenuItem
           name='create-tournament'
-          active={activeName==='create-tournament'}
+          active={this.state.activeName === 'create-tournament'}
+          onClick={() => this._navigate('/create-tournament')}
         >
           Create Tournament
         </MenuItem>
-      </Link>
-    </Menu>);
-
-};
+        <MenuMenu position='right'>
+          <MenuItem
+            name='signup'
+            onClick={() => this._navigate('/signup')}
+          >
+            <Button primary>
+              Sign up
+            </Button>
+          </MenuItem>
+          <MenuItem
+            name='login'
+            onClick={() => this._navigate('/login')}
+          >
+            <Button secondary>
+              Log in
+            </Button>
+          </MenuItem>
+        </MenuMenu>
+      </Menu>);
+  }
+}
 
 const NavigationBarWithRouter = withRouter(NavigationBar);
 
