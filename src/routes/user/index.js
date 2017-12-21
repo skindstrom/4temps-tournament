9d31b737db6cda40aa3a917b1fe923695ea603d7
Simon Kindström <simon.kindstrom@gmail.com>
2017-12-21 22:23:09 +0100

    Fix logout not working
    I guess I forgot to add the endpoint...

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -3,11 +3,13 @@ import { Router } from 'express';
 
 import createUser from './create-user';
 import loginUser from './login-user';
+import logoutUser from './logout-user';
 
 const router = Router();
 
 router.post('/create', createUser);
 
 router.post('/login', loginUser);
+router.post('/logout', logoutUser);
 
 export default router;
