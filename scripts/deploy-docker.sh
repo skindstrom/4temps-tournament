630f8bcbec017fbfd90b594b06c97f1da7e5104e
Simon Kindström <simon.kindstrom@gmail.com>
2018-01-19 20:10:25 +0100

    Ok I lied about travis + docker functioning

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -2,7 +2,7 @@
 
 # This script requires the environment variables $DOCKER_USERNAME and $DOCKER_SECRET
 
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_SECRET";
-docker build -t 4temps .;
-docker tag 4temps:latest ${DOCKER_USERNAME}/4temps:latest;
-docker push ${DOCKER_USERNAME}/4temps:latest;
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_SECRET"
+docker build -t 4temps .
+docker tag 4temps:latest ${DOCKER_USERNAME}/4temps:latest
+docker push ${DOCKER_USERNAME}/4temps:latest
