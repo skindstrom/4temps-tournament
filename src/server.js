f76f5c9e95f785af687e6d04e6cbf0d8cc78305e
Simon Kindström <simon.kindstrom@gmail.com>
2018-01-20 14:27:04 +0100

    Added favicon
    Also don't use /public for public files

--- src/server.js
+++ src/server.js
@@ -137,13 +137,13 @@ class Server {
 
   _enablePublicDirectoryRouting = () => {
     // used for files that should be public, but is static
-    this._app.use('/public', Express.static(path.join(__dirname, '../public'), {
-      maxAge: 1000 * 60 * 60 // 1 hour
+    this._app.use('/', Express.static(path.join(__dirname, '../public'), {
+      maxAge: 1000 * 60 * 60 * 24 // 1 day
     }));
     // used for files that should be public, but that's generated
-    this._app.use('/public',
+    this._app.use('/',
       Express.static(path.join(__dirname, '../public-build'), {
-        maxAge: 1000 * 60 * 60 // 1 hour
+        maxAge: 1000 * 60 * 60 // 1 day
       }));
   }
 
--- src/ssr-template.js
+++ src/ssr-template.js
@@ -9,7 +9,13 @@ export default (html: string, preloadedState: ReduxState, cspNonce: string) => {
       <head>
         <meta charset="UTF-8">
         <title>4 Temps</title>
-        <link rel="stylesheet" href="/public/app.css"></link>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+
+        <link rel="stylesheet" href="/app.css"></link>
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
       </head>
       <body>
@@ -17,7 +23,7 @@ export default (html: string, preloadedState: ReduxState, cspNonce: string) => {
         <script nonce="${cspNonce}" type="application/javascript">
           window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
         </script>
-        <script type="application/javascript" src="/public/bundle.js"></script>
+        <script type="application/javascript" src="/bundle.js"></script>
       </body>
     </html>
     `;
