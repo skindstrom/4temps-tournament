74649aec08e6cc73552ee9700269eefb1c8a6700
Simon Kindström <simon.kindstrom@gmail.com>
2018-01-21 11:04:15 +0100

    Added client API for creating round

---
+++ src/app/api/round/index.js
@@ -0,0 +1,18 @@
+// @flow
+
+import { apiPostRequest } from '../util';
+
+import validateRound from '../../../validators/validate-round';
+import type { RoundDbModel } from '../../../data/round';
+
+export const createRound =
+  async (tournamentId: string, round: Round): Promise<RoundDbModel> => {
+    const validation = validateRound(round);
+    if (!validation.isValidRound) {
+      throw validation;
+    }
+
+    return apiPostRequest('/api/participant/create');
+  };
+
+export default createRound;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ import { Router } from 'express';
 import UserRoute from './user';
 import TournamentRoute from './tournament';
 import ParticipantRoute from './participant';
+import RoundRoute from './round';
 
 const router = Router();
 
 router.use('/user', UserRoute);
 router.use('/tournament', TournamentRoute);
 router.use('/participant', ParticipantRoute);
+router.use('/round', RoundRoute);
 
 export default router;
