03289eb6be93362dde16090f1b9c5d898e693e78
Fredrik Rahn <rahn.fredrik@gmail.com>
2018-02-10 18:51:55 +0100

    Added api ep for changing attendance

--- src/app/api/participant/index.js
+++ src/app/api/participant/index.js
@@ -5,19 +5,33 @@ import { apiPostRequest } from '../util';
 import type { Participant } from '../../../models/participant';
 import { validateParticipant } from '../../../validators/validate-participant';
 
-export const createParticipant =
-  async (tournamentId: string,
-    participant: Participant): Promise<{
-      tournamentId: string, participant: Participant
-    }> => {
-    const validation = validateParticipant(participant);
-    if (!validation.isValidParticipant) {
-      throw validation;
-    }
+export const createParticipant = async (
+  tournamentId: string,
+  participant: Participant
+): Promise<{
+  tournamentId: string,
+  participant: Participant
+}> => {
+  const validation = validateParticipant(participant);
+  if (!validation.isValidParticipant) {
+    throw validation;
+  }
 
-    return apiPostRequest(
-      `/api/participant/${tournamentId}/create`,
-      { tournamentId, participant });
-  };
+  return apiPostRequest(`/api/participant/${tournamentId}/create`, {
+    tournamentId,
+    participant
+  });
+};
+
+export async function changeAttendance(
+  tournamentId: string,
+  participantId: string,
+  isAttending: boolean
+) {
+  return apiPostRequest(`/api/participant/${tournamentId}/attendance`, {
+    participantId,
+    isAttending
+  });
+}
 
 export default createParticipant;
