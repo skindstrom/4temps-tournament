e7f8e7c1bdf62aed0efaa62b13bc1e11f9b2e9b1
Fredrik Rahn <rahn.fredrik@gmail.com>
2018-02-10 18:50:30 +0100

    Added validator for checking attendance

--- src/validators/__tests__/validate-participant.js
+++ src/validators/__tests__/validate-participant.js
@@ -3,27 +3,54 @@
 import { validateParticipant } from '../validate-participant';
 
 test('An empty name is invalid', () => {
-  expect(validateParticipant({ id: '', name: '', role: 'leader' }))
-    .toMatchObject({
-      isValidParticipant: false,
-      isValidName: false
-    });
+  expect(
+    validateParticipant({ id: '', name: '', role: 'leader', isAttending: true })
+  ).toMatchObject({
+    isValidParticipant: false,
+    isValidName: false
+  });
 
-  expect(validateParticipant({ id: '', name: 'sweet', role: 'leader' }))
-    .toMatchObject({ isValidName: true });
+  expect(
+    validateParticipant({
+      id: '',
+      name: 'sweet',
+      role: 'leader',
+      isAttending: true
+    })
+  ).toMatchObject({ isValidName: true });
 });
 
 test('Any role other than "none" is valid', () => {
-  expect(validateParticipant({ id: '', name: 'name', role: 'none' }))
-    .toMatchObject({ isValidParticipant: false, isValidRole: false });
-  expect(validateParticipant({ id: '', name: 'name', role: 'leader' }))
-    .toMatchObject({ isValidRole: true });
-  expect(validateParticipant({ id: '', name: 'name', role: 'follower' }))
-    .toMatchObject({ isValidRole: true });
-  expect(validateParticipant({
-    id: '',
-    name: 'name',
-    role: 'leaderAndFollower'
-  }))
-    .toMatchObject({ isValidRole: true });
+  expect(
+    validateParticipant({
+      id: '',
+      name: 'name',
+      role: 'none',
+      isAttending: true
+    })
+  ).toMatchObject({ isValidParticipant: false, isValidRole: false });
+  expect(
+    validateParticipant({
+      id: '',
+      name: 'name',
+      role: 'leader',
+      isAttending: true
+    })
+  ).toMatchObject({ isValidRole: true });
+  expect(
+    validateParticipant({
+      id: '',
+      name: 'name',
+      role: 'follower',
+      isAttending: true
+    })
+  ).toMatchObject({ isValidRole: true });
+  expect(
+    validateParticipant({
+      id: '',
+      name: 'name',
+      role: 'leaderAndFollower',
+      isAttending: true
+    })
+  ).toMatchObject({ isValidRole: true });
 });
