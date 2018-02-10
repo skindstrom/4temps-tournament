3368889d6ce1ffc89c40517558b0584ee1d54d5c
samhal <halali@student.chalmers.se>
2018-02-10 18:48:33 +0100

    Add reroute to judge page after judge authentication

--- src/app/components/Login/AccessKeyLogin/index.js
+++ src/app/components/Login/AccessKeyLogin/index.js
@@ -1,19 +1,26 @@
 //@flow
 import { connect } from 'react-redux';
+import type { RouterHistory } from 'react-router-dom';
 import { loginJudge } from '../../../api/judge';
 import LoginComponent from './component';
 
+type Props = {
+  history: RouterHistory
+}
 
 function mapStateToProps({ ui }: ReduxState) {
   return ui.judgeLogin;
 }
 
-function mapDispatchToProps(dispatch: ReduxDispatch) {
+function mapDispatchToProps(dispatch: ReduxDispatch, {history}: Props) {
   return {
     onSubmit: (accessKey: string) =>
       dispatch({
         type: 'LOGIN_WITH_ACCESS_KEY',
         promise: loginJudge(accessKey),
+        meta: {
+          onSuccess: () => history.push('/judge')
+        }
       })
   };
 }
