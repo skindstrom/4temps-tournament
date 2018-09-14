a8db3be24fbb37088262fcff8a4363b9fa5a46a6
Simon Kindström <simon.kindstrom@gmail.com>
2018-09-14 09:36:48 +0200

    Display judge type in UI
    Related to #136

--- src/app/components/EditTournament/Judges/ListJudges/component.js
+++ src/app/components/EditTournament/Judges/ListJudges/component.js
@@ -20,6 +20,7 @@ function ListJudges({ judges }: { judges: Array<JudgeViewModel> }) {
       <TableHeader>
         <TableRow>
           <TableHeaderCell>Name</TableHeaderCell>
+          <TableHeaderCell>Type</TableHeaderCell>
           <TableHeaderCell>Access Key</TableHeaderCell>
         </TableRow>
       </TableHeader>
@@ -27,12 +28,23 @@ function ListJudges({ judges }: { judges: Array<JudgeViewModel> }) {
         {judges.map(j => (
           <TableRow key={j.id}>
             <TableCell>{j.name}</TableCell>
+            <TableCell>{typeToDisplayName(j.type)}</TableCell>
             <TableCell>{j.accessKey}</TableCell>
           </TableRow>
         ))}
       </TableBody>
     </Table>
   );
+}
+
+function typeToDisplayName(type: JudgeType): string {
+  if (type === 'normal') {
+    return 'Normal';
+  } else if (type === 'sanctioner') {
+    return 'Sanctioner';
+  } else {
+    return 'Unknown';
+  }
 }
 
 export default ListJudges;
